8f51e78274eaec883e00dfa6a72ea6a224ec1515
Jacobo <jacob.et.cetera@gmail.com>
2017-04-29 03:01:55 -0500

    Finaliza

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ from ReadData import read_data
 from NeuralNetwork import get_results as neural_get_results
 from sklearn.model_selection import train_test_split
 from NearestNeighbors import get_results as neig_get_results
+import csv
 
 data,features = read_data()
 y = data["Performance"]
@@ -24,8 +25,21 @@ media_de_error += neural_get_results(
     X_test, y_train, y_test,x,y)
 print ("\n----------")
 
-neig_get_results(data.copy(),features,X_train,
+a,b,c = neig_get_results(data.copy(),features,X_train,
     X_test, y_train, y_test,x,y)
 
+media_de_error += b
+print ("\n----------\n")
 
+#print ("Media de error de los resultados: "+str((media_de_error/3)))
+
+
+resultado = 0
+with open("global_results.csv") as f:
+    for row in csv.reader(f):
+        calc2 = float(row[0]) - float(row[4])
+        print (calc2*calc2)
+        resultado+=(calc2*calc2)
+
+print ("Error cuadrático medio entre la columna 1 y la columna 5: "+str(float(resultado*0.5)))
 
--- NearestNeighbors.py
+++ NearestNeighbors.py
@@ -20,6 +20,16 @@ def get_results(data,features,X_train,X_test,y_train,y_test,x,y):
         print (line,file = f)
     f.close()
 
+    resultA = 0
+    with open("3neighborgs.csv") as f:
+        total = 0
+        for row in csv.reader(f):
+            calc = float(row[0]) - float(row[1])
+            total += (calc * calc)
+        print("Error cuadrático medio(K=3): " + str(0.5 * calc))
+        print ("\n--------------")
+        resultA = (calc*0.5)
+
     siete_vecinos = KNeighborsClassifier(n_neighbors=7)
     siete_vecinos.fit(X_train,y_train)
     siete_predicted = siete_vecinos.predict(X_test)
@@ -31,6 +41,16 @@ def get_results(data,features,X_train,X_test,y_train,y_test,x,y):
         aux += 1
         print (line,file = f)
     f.close()
+
+    resultC = 0
+    with open("7neighborgs.csv") as f:
+        total = 0
+        for row in csv.reader(f):
+            calc = float(row[0]) - float(row[1])
+            total += (calc * calc)
+        print("Error cuadrático medio(K=7): " + str(0.5 * calc))
+        print ("\n--------------")
+        resultC = (calc*0.5)
 
     cinco_vecinos = KNeighborsClassifier(n_neighbors=5)
     cinco_vecinos.fit(X_train,y_train)
@@ -46,16 +66,33 @@ def get_results(data,features,X_train,X_test,y_train,y_test,x,y):
         print (line,file = f)
     f.close()
 
+    resultB = 0
+    with open("5neighborgs.csv") as f:
+        total = 0
+        for row in csv.reader(f):
+            calc = float(row[0]) - float(row[1])
+            total += (calc * calc)
+        print("Error cuadrático medio(K=5): " + str(0.5 * calc))
+        print ("\n--------------")
+        resultB = (calc*0.5)
+
 
     lines = []
+    acumulado = []
     with open("global_results.csv") as f:
         for row in csv.reader(f):
             calc = str(row[0])+","+str(row[1])+","+str(row[2])
+            calc2 = float(row[1])+float(row[2])
+            acumulado.append(calc2)
             lines.append(calc)
+
     aux = 0
     f = open('global_results.csv', 'w')
     for i in range(0,len(lines)):
-        line =lines[aux] + ","+ str(cinco_predicted[aux])
+        mean = acumulado[aux] + float(cinco_predicted[aux])
+        line = lines[aux] + ","+ str(cinco_predicted[aux]) + "," + str((mean/3))
         aux += 1
         print (line,file = f)
     f.close()
+
+    return resultA,resultB,resultC
